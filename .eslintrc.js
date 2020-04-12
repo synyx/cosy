908555f83882333ab1256e3e29eac86b3c199fb3
Benjamin Seber <benjamin.seber@mailbox.org>
2020-04-12 15:26:59 +0200

    add ldap login
    note: open source; project has been rebased to remove credential stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: "eslint:recommended",
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: "module",
+	},
+	rules: {},
+	overrides: [
+		{
+			files: ["src/**/*.js"],
+			env: {
+				node: true,
+			},
+		},
+	],
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,109 @@
+const fs = require("fs");
 const path = require("path");
 const Koa = require("koa");
+const views = require("koa-views");
+const bodyParser = require("koa-bodyparser");
+const passport = require("koa-passport");
+const session = require("koa-session");
+const route = require("koa-route");
 const serve = require("koa-static");
 const websockify = require("koa-websocket");
+const LdapStrategy = require("passport-ldapauth");
+
+const { PORT = 3000, APP_SECRET = "app-secret" } = process.env;
 
 const app = websockify(new Koa());
+app.use(bodyParser());
 
+// authentication
+passport.use(
+	new LdapStrategy({
+		server: {
+			url: "ldap://---------------",
+			searchBase: "----------------",
+			searchFilter: "(uid={{username}})",
+		},
+		// form post requestBody field names
+		usernameField: "username",
+		passwordField: "password",
+	}),
+);
+app.keys = [APP_SECRET];
+app.use(session({}, app));
+app.use(passport.initialize());
+// app.use(passport.session());
+
+// Must be used before any router is used
+app.use(
+	views(path.resolve(__dirname, "views"), {
+		extension: "hbs",
+		map: { hbs: "handlebars" },
+	}),
+);
+
+// ensure authenticated access
+app.use(async function (ctx, next) {
+	if (
+		ctx.isAuthenticated() ||
+		ctx.originalUrl.startsWith("/login") ||
+		ctx.originalUrl.startsWith("/css/")
+	) {
+		await next();
+	} else {
+		console.log("not authenticated. redirecting to /login");
+		ctx.redirect("/login");
+	}
+});
+
+// static assets
 const assetsRootDir = path.resolve(__dirname, "../static");
 app.use(serve(assetsRootDir));
+
+// ---------------------------------------------------------
+// LOGIN
+app.use(
+	route.get("/login", async function (ctx) {
+		console.log(":: /login");
+		console.log(":: ", ctx);
+		const { error, username } = ctx.request.query;
+		await ctx.render("login", {
+			error: error != undefined,
+			username,
+		});
+	}),
+);
+app.use(
+	route.post("/login", async function (ctx, next) {
+		const { username } = ctx.request.body;
+		await passport.authenticate("ldapauth", {
+			session: false,
+			successRedirect: "/",
+			failureRedirect: `/login?error&username=${username}`,
+		})(ctx, next);
+	}),
+);
+
+// ---------------------------------------------------------
+// LOGOUT
+//
+app.use(
+	route.post("/logout", async function (ctx) {
+		await ctx.logout();
+		ctx.rdirect("/");
+	}),
+);
+
+// ---------------------------------------------------------
+// GAME
+//
+app.use(
+	route.get("/", function (ctx) {
+		ctx.type = "html";
+		ctx.body = fs.createReadStream(
+			path.resolve(__dirname, "../templates/index.html"),
+		);
+	}),
+);
 
 let users = [];
 
@@ -43,6 +140,6 @@ app.ws.use((ctx) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log("server is listening in http://localhost:3000");
+app.listen(PORT, () => {
+	console.log(`server is listening in http://localhost:${PORT}`);
 });
