42a3d1b5d5b23ddca348278ecf04a498eb10a535
Benjamin Seber <benjamin.seber@mailbox.org>
2020-04-13 21:43:40 +0200

    add content-security-policy headers

--- src/server/index.js
+++ src/server/index.js
@@ -18,11 +18,71 @@ app.use(new CSRF());
 app.use(helmet());
 
 // install view resolver
-// Must be used before any router is used
-app.use(
-	views(path.resolve(__dirname, "templates"), {
+// Must be used
+// - before any router is used
+// - before CSP middleware
+//   (as we're generating nonce value here which are used by the CSP middleware)
+app.use(function viewResolver(context, next) {
+	// cache generated nonce values in the context state
+	// these are handled by helmet-csp middleware later
+	context.res.state = context.res.state || {};
+	context.res.state.csp = context.res.state.csp || {};
+	context.res.state.csp.nonce = context.res.state.csp.nonce || {};
+	context.res.state.csp.nonce.script = context.res.state.csp.nonce.script || [];
+	context.res.state.csp.nonce.style = context.res.state.csp.nonce.style || [];
+
+	return views(path.resolve(__dirname, "templates"), {
 		extension: "hbs",
 		map: { hbs: "handlebars" },
+		options: {
+			helpers: {
+				cspNonceValue: (type) => {
+					const value = require("crypto").randomBytes(16).toString("base64");
+					switch (type) {
+						case "script": {
+							context.res.state.csp.nonce.script.push(value);
+							return value;
+						}
+						case "style": {
+							context.res.state.csp.nonce.style.push(value);
+							return value;
+						}
+						default:
+							console.log(
+								`cannot handle type=${type} to generate a nonce value. must be of type [script|style]`,
+							);
+							return "";
+					}
+				},
+			},
+		},
+	})(context, next);
+});
+
+// install CSP middleware
+// must be after the view resolver. as the view resolver writes nonce values
+// into the response state
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: ["'self'"],
+			scriptSrc: [
+				"'self'",
+				(request, response) => {
+					return response.state.csp.nonce.script.map((v) => `'nonce-${v}'`);
+				},
+			],
+			styleSrc: [
+				"'self'",
+				(request, response) => {
+					return response.state.csp.nonce.style.map((v) => `'nonce-${v}'`);
+				},
+			],
+			fontSrc: ["'self'"],
+			imgSrc: ["'self'"],
+			objectSrc: ["'none'"],
+			sandbox: ["allow-scripts", "allow-forms", "allow-same-origin"],
+		},
 	}),
 );
 
