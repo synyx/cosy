545faf6ba241ee309d4d8dddb467ed4009d963eb
Benjamin Seber <benjamin.seber@mailbox.org>
2020-04-18 10:58:09 +0200

    don't stop player pulse animation when clicking anywhere on the document

--- static/js/app.js
+++ static/js/app.js
@@ -42,11 +42,10 @@ document
 	});
 
 document.body.addEventListener("click", (event) => {
-	stopPlayerAvatarAnimate();
-
 	if (actionMenu.contains(event.target)) {
 		//
 	} else if (event.target === playerAvatar) {
+		stopPlayerAvatarAnimate();
 		actionMenu.classList.remove("hidden");
 
 		const { pageX: x, pageY: y } = event;
