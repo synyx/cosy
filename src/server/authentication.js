898769142ce7fae8db886fde6e0ed8af9285fff8
Benjamin Seber <benjamin.seber@mailbox.org>
2020-04-22 23:22:09 +0200

    intruce admin page to add new users on demand

--- src/server/authentication.js
+++ src/server/authentication.js
@@ -1,11 +1,28 @@
 const session = require("koa-session");
 const passport = require("koa-passport");
 const LdapStrategy = require("passport-ldapauth");
+const LocalStrategy = require("passport-local");
+const crypto = require("crypto");
+const path = require("path");
+const fs = require("fs");
 
 const {
 	LDAP_SERVER_URL: ldapServerUrl,
 	LDAP_SERVER_SEARCHBASE: ldapServerSearchBase,
 } = process.env;
+
+let localUsers = {};
+const adminApprovedUsers = new Map();
+
+const localUsersFilePath = path.resolve(__dirname, "../../local-users.json");
+try {
+	const localUsersFile = fs.readFileSync(localUsersFilePath, "utf8");
+	localUsers = JSON.parse(localUsersFile);
+} catch (error) {
+	console.log(
+		`[WARN] could not load local user file '${localUsersFilePath}'. Only Ldap is active now.`,
+	);
+}
 
 passport.serializeUser((user, next) => {
 	next(null, {
@@ -35,43 +52,35 @@ passport.use(
 	}),
 );
 
-if (process.env.NODE_ENV === "development") {
-	let localUsers = {};
-
-	const LocalStrategy = require("passport-local");
-	const path = require("path");
-	const fs = require("fs");
-	const localUsersFilePath = path.resolve(__dirname, "../../local-users.json");
-
-	try {
-		const localUsersFile = fs.readFileSync(localUsersFilePath, "utf8");
-		localUsers = JSON.parse(localUsersFile);
-	} catch (error) {
-		console.log(
-			`[WARN] could not load local user file '${localUsersFilePath}'. Only Ldap is active now.`,
-		);
-	}
-
-	console.log(`configure local auth [usernames=${Object.keys(localUsers)}]`);
-	passport.use(
-		new LocalStrategy(
-			{
-				session: true,
-				// form post requestBody field names
-				usernameField: "username",
-				passwordField: "password",
-			},
-			function (username, password, done) {
+console.log(
+	`configure adminApprovedUser auth. add users with the admin board.`,
+);
+passport.use(
+	new LocalStrategy(
+		{
+			session: true,
+			// form post requestBody field names
+			usernameField: "username",
+			passwordField: "password",
+		},
+		function (username, password, done) {
+			const approvedUser = adminApprovedUsers.get(username);
+			if (approvedUser && approvedUser.password === password) {
+				done(null, approvedUser);
+			} else if (process.env.NODE_ENV === "development") {
+				console.log(`check local auth [usernames=${Object.keys(localUsers)}]`);
 				const user = localUsers[username];
 				if (user) {
 					done(null, user);
 				} else {
 					done(null, false);
 				}
-			},
-		),
-	);
-}
+			} else {
+				done(null, false);
+			}
+		},
+	),
+);
 
 module.exports = function (app) {
 	app.use(session({}, app));
@@ -93,4 +102,22 @@ module.exports = function (app) {
 			context.redirect("/login");
 		}
 	});
+};
+
+module.exports.addAdminApprovedUser = function addAdminApprovedUser({
+	cn,
+	email,
+	synyxNickname,
+}) {
+	const password = crypto.randomBytes(20).toString("hex").substring(0, 16);
+	adminApprovedUsers.set(synyxNickname, {
+		cn,
+		mail: email,
+		synyxNickname,
+		password,
+	});
+};
+
+module.exports.getAdminApprovedUsers = function getAdminApprovedUsers() {
+	return [...adminApprovedUsers.values()];
 };
---
+++ src/server/routes/admin.js
@@ -0,0 +1,66 @@
+const route = require("koa-route");
+const {
+	addAdminApprovedUser,
+	getAdminApprovedUsers,
+} = require("../authentication");
+
+module.exports = function (app) {
+	function auth(callback) {
+		return function (context) {
+			if (context.state.user.email !== "seber@synyx.de") {
+				context.response.status = 403;
+				return context.render("403");
+			}
+			return callback(context);
+		};
+	}
+
+	app.use(
+		route.get(
+			"/admin",
+			auth(function (context) {
+				const players = getAdminApprovedUsers();
+				return context.render("admin", {
+					csrf: context.csrf,
+					players,
+				});
+			}),
+		),
+	);
+
+	app.use(
+		route.get(
+			"/admin/users",
+			auth(function (context) {
+				const players = getAdminApprovedUsers();
+				return context.render("admin", {
+					csrf: context.csrf,
+					players,
+				});
+			}),
+		),
+	);
+
+	app.use(
+		route.post(
+			"/admin/users",
+			auth(function (context) {
+				const { cn, email, synyxNickname } = context.request.body;
+
+				if (!cn || !email || !synyxNickname) {
+					return context.render("admin", {
+						csrf: context.csrf,
+						players: getAdminApprovedUsers(),
+						error: true,
+						cn,
+						email,
+						synyxNickname,
+					});
+				}
+
+				addAdminApprovedUser({ cn, email, synyxNickname });
+				context.response.redirect("/admin");
+			}),
+		),
+	);
+};
