053433c66d66eee0b8b1f3e61180d102201e476e
Benjamin Seber <benjamin.seber@mailbox.org>
2020-05-05 20:53:20 +0200

    stop toilet flush sound when leaving toilet

--- src/browser/actions/toilet-action.js
+++ src/browser/actions/toilet-action.js
@@ -1,24 +1,48 @@
 export function createToiletActions({ send, player, playerAvatar }) {
 	let flushingToiletCounter = 0;
+	let flushingAudio = [];
 
 	function playSound(src) {
+		const audio = new Audio(src);
+		flushingAudio.push(audio);
 		return new Promise((resolve) => {
-			const audio = new Audio(src);
-			audio.addEventListener("ended", resolve);
+			audio.addEventListener("ended", () => {
+				flushingAudio = flushingAudio.filter((a) => a !== audio);
+				resolve();
+			});
 			audio.load();
 			audio.play();
 		});
 	}
 
 	return {
-		handleRoomChange({ previousRoom, nextRoom }) {},
+		handleRoomChange({ previousRoom, nextRoom }) {
+			if (
+				(previousRoom.id === "floor_toilet_m_1" &&
+					nextRoom.id === "floor_main") ||
+				(previousRoom.id === "floor_toilet_f_1" &&
+					nextRoom.id === "floor_billard")
+			) {
+				for (let audio of flushingAudio) {
+					audio.pause();
+					audio.currentTime = 0;
+				}
+				flushingAudio = [];
+			}
+		},
 
 		handleWebsocket(type, content, { currentRoomName }) {
 			switch (type) {
 				case "toilet-flush": {
 					const { roomName, userName } = content;
 					if (flushingToiletCounter < 2) {
-						if (userName !== player.name && roomName === currentRoomName) {
+						if (
+							userName !== player.name &&
+							// same room
+							(roomName === currentRoomName ||
+								// or antechamber
+								currentRoomName.startsWith(roomName))
+						) {
 							playSound("/sounds/46274__phreaksaccount__flush1.mp3");
 						}
 					}
