4f982e3d8bb66442b16673ae5f807b64fb5ee8c9
Benjamin Seber <benjamin.seber@mailbox.org>
2020-05-06 21:08:11 +0200

    style living-room action board

--- src/browser/actions/living-room-action.js
+++ src/browser/actions/living-room-action.js
@@ -1,4 +1,8 @@
 export function createLivingRoomActions({ send, player, playerAvatar }) {
+	const root = document.querySelector("#living-room-tv-root");
+	root.style.transition = "background 1s ease-out";
+	root.style.backgroundColor = "rgba(0,0,0,0)";
+
 	return {
 		handleRoomChange({ previousRoom, nextRoom }) {},
 
@@ -16,14 +20,24 @@ export function createLivingRoomActions({ send, player, playerAvatar }) {
 				},
 
 				handleSelect({ currentRoom, attrs }) {
-					const root = document.querySelector("#living-room-tv-root");
 					const inner = document.querySelector("#living-room-tv-root-inner");
 					const closeButton = document.querySelector("#close-game-button");
 
+					root.style.backgroundColor = "rgba(0,0,0,0.25)";
+					root.classList.add("z-50");
+					root.classList.remove("hidden");
+
+					setTimeout(function () {
+						inner.classList.add("w-full");
+						inner.style.height = "100%";
+					}, 0);
+
 					closeButton.addEventListener("click", (event) => {
-						inner.classList.remove("w-full", "h-full");
+						root.style.backgroundColor = "rgba(0,0,0,0)";
+						root.classList.remove("z-50", "w-full");
 						root.classList.add("hidden");
-						root.classList.remove("z-50");
+						inner.style.transition = "";
+						inner.style.height = "";
 					});
 
 					root.classList.remove("hidden");
