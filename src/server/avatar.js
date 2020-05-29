f2ffeb239e639445d51db893fdf0cd2320a373e0
Benjamin Seber <benjamin.seber@mailbox.org>
2020-05-29 10:54:37 +0200

    cache generated avatarurl for the given identifier

--- src/server/avatar.js
+++ src/server/avatar.js
@@ -1,4 +1,11 @@
+const avatarMap = new Map();
+
 module.exports.getAvatarUrl = function getAvatarUrl(email, options) {
+	if (avatarMap.has(email)) {
+		return avatarMap.get(email);
+	}
 	const randomNumber = Math.round(Math.random() * 10);
-	return `/avatars/avataaars-${randomNumber}.png`;
+	const url = `/avatars/avataaars-${randomNumber}.png`;
+	avatarMap.set(email, url);
+	return url;
 };
